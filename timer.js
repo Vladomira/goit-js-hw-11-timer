114e76dc830ecdb2182a7474dcd8be7a66a57368
Vladomira <juliavladomira@gmail.com>
2021-08-08 15:34:24 +0300

    resolve the mistake

--- timer.js
+++ timer.js
@@ -1,44 +1,45 @@
 
 
 class CountdownTimer {
-    consrtuctor({ selector, targetDate }){        
+    constructor({ selector, targetDate }){
+            
         this.targetDate = targetDate;
-        this.selector = selector;        
+        // this.selector = selector;        
         this.selectorTimer = document.querySelector(selector);
-
-        // this.daysText = document.querySelector(`${selector} [data-value="days"]`).textContent;
-        this.daysText = this.selectorTimer.children[0].children[0].textContent;
-        this.hoursText = this.selectorTimer.children[1].children[0].textContent;
-        this.minsText =  this.selectorTimer.children[2].children[0].textContent;
-        this.secsText =  this.selectorTimer.children[3].children[0].textContent;       
+       
+        this.daysText = this.selectorTimer.children[0].children[0];
+        this.hoursText = this.selectorTimer.children[1].children[0];
+        this.minsText =  this.selectorTimer.children[2].children[0];
+        this.secsText =  this.selectorTimer.children[3].children[0]; 
     }
     
     timer(){
-    const futureTime = new Date("Sep 5, 2022").getTime(); 
+    const futureTime = this.targetDate.getTime(); 
     
     setInterval(() =>{
         const currentTime = Date.now();     
         const deltaTime = futureTime - currentTime;
-        console.log(deltaTime);
+        // console.log(deltaTime);
         const days = Math.floor(deltaTime / (1000 * 60 * 60 * 24));
         const hours = Math.floor((deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const mins = Math.floor((deltaTime % (1000 * 60 * 60)) / (1000 * 60));
         const secs = Math.floor((deltaTime % (1000 * 60)) / 1000);
 
-        this.daysText = days  < 10 ? `0${days}` : days; 
-        this.hoursText = hours  < 10 ? `0${hours}` : hours; 
-        this.minsText = mins  < 10 ? `0${mins}` : mins;
-        this.secsText = secs  < 10 ? `0${secs}` : secs;
-
-        return `${days}:${hours}:${mins}:${secs}`;   
-        // console.log( this.selectorTimer)     
+        this.daysText.textContent = days  < 10 ? `0${days}` : days; 
+        this.hoursText.textContent = hours  < 10 ? `0${hours}` : hours; 
+        this.minsText.textContent = mins  < 10 ? `0${mins}` : mins;
+        this.secsText.textContent = secs  < 10 ? `0${secs}` : secs;
+        
+        return `${days}:${hours}:${mins}:${secs}`;  
+            
     }, 1000);
     }      
 }
 
 const countdown = new CountdownTimer({
     selector: "#timer-1",
-    targetDate: Date.now(),
+    targetDate: new Date("Sep 5, 2022"),
+    // targetDate: Date.now(),
     }
 )
 
